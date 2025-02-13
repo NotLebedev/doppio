b0296b8a5cd44fc8f201142b6cd9f3da837acb33
NotLebedev <notlebedev@gmail.com>
2025-02-13 15:12:47 +0300

    Remove dependency on futures and futures-locks

--- src/bin/doppio-daemon/state.rs
+++ src/bin/doppio-daemon/state.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use anyhow::Result;
-use futures_locks::RwLock;
+use tokio::sync::RwLock;
 use zbus::{proxy, zvariant::OwnedFd, Connection};
 pub struct State<'a> {
     proxy: ManagerProxy<'a>,
